6046152f50aad93189d0197f3f04cb266871bab8
sean <seanschmidt89@yahoo.com>
2023-02-21 00:10:37 -0800

    adding hero, sidenav, work, workitem comps and css

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SideNav.css";
+
+const SideNav = () => {
+  return (
+    <div className="sidenav">
+      <div className="small-nav">
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+      </div>
+      <div className="large-nav">
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
---
+++ src/components/WorkItem/WorkItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./WorkItem.css";
+
+const WorkItem = () => {
+  return <div></div>;
+};
+
+export default WorkItem;
